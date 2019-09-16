9e539a7f3c3bf4e6d6361853acb803b57c945fe1
johnq0317 <johnq0317@tamu.edu>
2019-09-15 20:13:22 -0500

    all functions but pt 3 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -11,23 +11,23 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return true if arr.combination(2).find{|x, y| x + y == n}
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
   'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
-  s[0] =~ /[^aeiou]i/
+  s =~ /\A(?=[^aeiou])([a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s !~ /[^01]/ and s.length > 0 and s.to_i(2) % 4 == 0
+  false
 end
 
 # Part 3
